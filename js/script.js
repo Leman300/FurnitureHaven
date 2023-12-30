230bc2245a98bfa7777eca3837d758a4f81f0621
Bartosz Lemanek <82775580+Leman300@users.noreply.github.com>
2023-12-30 14:30:12 +0100

    Added whole project

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const nav = document.querySelector('.navbar-collapse')
+const footerYear = document.querySelector('.footer__year');
+const collectionYear = document.querySelector('.collection-year');
+const btns = document.querySelector('.btn')
+
+
+const buttonAlert = () => {
+  alert("It's only UI Design\nThere is no backend there");
+}
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+    collectionYear.innerText = year;
+}
+handleCurrentYear();
+
+document.addEventListener('click', () => {
+  if (nav.classList.contains('show')) {
+    nav.classList.remove('show')
+  }
+})
+
+btns.addEventListener('click', buttonAlert)
